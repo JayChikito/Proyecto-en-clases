0791da435123830c9a57db77fa115c8ec18440bd
Jay <jaychikito@gmail.com>
2022-11-12 16:08:07 -0500

    Se agregaron las clases para un sistema de facturacion en el paquete modelos

--- src/modelo/DetalleFactura.java
+++ src/modelo/DetalleFactura.java
@@ -11,8 +11,60 @@ package modelo;
  */
 public class DetalleFactura {
 
-        private Long id;
+    private Long id;
+    private Factura factura;
+    private Producto producto;
     private Integer cantidad;
     private Float pUnitario;
-    private float pTotal;
+    private Float pTotal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Float getpUnitario() {
+        return pUnitario;
+    }
+
+    public void setpUnitario(Float pUnitario) {
+        this.pUnitario = pUnitario;
+    }
+
+    public Float getpTotal() {
+        return pTotal;
+    }
+
+    public void setpTotal(Float pTotal) {
+        this.pTotal = pTotal;
+    }
+    
+    
 }
--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -10,5 +10,99 @@ package modelo;
  * @author DEEPIN
  */
 public class Persona {
+    private Integer id;
+    private String apellido;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String correo;
+    private String indetifiacion;
+    private String tipoIdentificacion;
+    private Boolean estado;
+    private Rol rol;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getIndetifiacion() {
+        return indetifiacion;
+    }
+
+    public void setIndetifiacion(String indetifiacion) {
+        this.indetifiacion = indetifiacion;
+    }
+
+    public String getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(String tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+
+    
+    
     
 }
